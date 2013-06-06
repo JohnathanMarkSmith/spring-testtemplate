60b2e57800cf596eadf15b973d7ec0875efb4d33
Johnathan Smith <johnathan.smith@uftwf.org>
2013-06-06 13:19:00 -0400

    added so more code

--- src/main/java/com/johnathanmarksmith/springresttemplate/Main.java
+++ src/main/java/com/johnathanmarksmith/springresttemplate/Main.java
@@ -1,8 +1,11 @@
 package com.johnathanmarksmith.springresttemplate;
 
 
+import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.log4j.Logger;
+import org.springframework.http.client.CommonsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import static org.apache.log4j.Logger.getLogger;
@@ -18,10 +21,22 @@ public class Main
     {
         LOGGER.debug("Starting REST Client!!!!");
 
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("test", "test");
+        // UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("test", "test");
 
 
-        RestTemplate restTemplate = new RestTemplate();
+        HttpClient client = new HttpClient();
+
+        UsernamePasswordCredentials credentials =
+                new UsernamePasswordCredentials("test","test");
+
+        client.getState().setCredentials(
+                new AuthScope("127.0.0.1", 8080, AuthScope.ANY_REALM),
+                credentials);
+
+        CommonsClientHttpRequestFactory commons = new CommonsClientHttpRequestFactory(client);
+
+        RestTemplate restTemplate = new RestTemplate(commons);
+
 
         String jsonreturn = restTemplate.getForObject("http://127.0.0.1:8080/springmvc-rest-secured-test/json/Regan", String.class);
 
